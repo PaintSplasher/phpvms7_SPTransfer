719102aa01b01df5cf52400a0219f9e2efa1e0a8
B.Fatih KOZ <fatih.koz@gmail.com>
2024-07-04 15:21:20 +0300

    Update DB_SPTransfer.php

--- Models/DB_SPTransfer.php
+++ Models/DB_SPTransfer.php
@@ -4,18 +4,62 @@ namespace Modules\SPTransfer\Models;
 
 use App\Contracts\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Kyslik\ColumnSortable\Sortable;
 
 class DB_SPTransfer extends Model
 {
-    use HasFactory;
+    use HasFactory, Sortable;
 
     protected $table = 'sptransfer';
 
     protected $fillable = [
         'user_id',
-        'hub_initial',
-        'hub_request',
+        'hub_initial_id',
+        'hub_request_id',
         'reason',
         'state',
     ];
+
+    // Technically not needed for default fields but here for exampling purposes
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+        // 'deleted_at' => 'datetime',
+    ];
+
+    // Better to define them here too, should follow the DB structure and code, gives an insight about stuff easily
+    public static $rules = [
+        'user_id'        => 'required',
+        'hub_initial_id' => 'optional|max:5',
+        'hub_request_id' => 'required|max:5',
+        'reason'         => 'nullable',
+        'state'          => 'nullable',
+    ];
+
+    // Allows sortable columns in views without performance loss
+    public $sortable = [
+        'user_id',
+        'hub_initial_id',
+        'hub_request_id',
+        'reason',
+        'state',
+    ];
+
+    // User Model Relationship
+    public function user(): HasOne
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    // Airport Model Relationship - Current Hub
+    public function hub_initial(): HasOne
+    {
+        return $this->hasOne(Airport::class, 'id', 'hub_initial_id');
+    }
+
+    // Airport Model Relationship - Requested Hub
+    public function hub_request(): HasOne
+    {
+        return $this->hasOne(Airport::class, 'id', 'hub_request_id');
+    }
 }
