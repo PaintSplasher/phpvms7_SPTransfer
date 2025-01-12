40d26f715f7b51227268ca6e090bbe99cc545c36
Thorsten <39538545+PaintSplasher@users.noreply.github.com>
2025-01-13 00:41:33 +0100

    pt-pt added.

---
+++ Resources/lang/pt-pt/common.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+   // Português Europeu
+   'title'     => 'Pedido de Transferência de HUB',
+   'based'     => 'Você está sediado em',
+   'limited'   => 'Você só pode solicitar uma pedido de transferência de HUB a cada',
+   'days'      => 'dia(s)',
+   'desired'   => 'HUB pretendido',
+   'reason'    => 'Motivo para a transferência de HUB',
+   'charged'   => 'Você só será cobrado',
+   'ontransfer' => 'apenas se o seu pedido for aceite',
+   'onrequest' => 'imediatamente para cada pedido enviado enviada',
+   'request'   => 'Solicitar uma Transferência',
+   'lasttitle' => 'Último Pedido de Transferência de HUB',
+   'didnot'    => 'Você não fez nenhum pedido de transferência de HUB.',
+   'transferid' => 'ID de Transferência',
+   'reqhub'    => 'HUB Solicitado',
+   'reqdate'   => 'Data do Pedido',
+   'reqstatus' => 'Situação do Pedido',
+   'reqreason' => 'Motivo do Pedido',
+   'reqis'     => 'O seu pedido está',
+   'reqform'   => 'Formulário de Pedido',
+   'chars'     => 'máximo de 100 caracteres',
+   'notenough' => 'Saldo insuficiente para realizar um pedido de transferência de HUB. Você precisa de',
+   'selectplace' => 'Digite para começar a busca',
+];
